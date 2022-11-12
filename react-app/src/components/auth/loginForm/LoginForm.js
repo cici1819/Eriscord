f7c2eab718c2da7bff5b17189af522f701498583
cici1819 <cchj1819@gmail.com>
2022-11-12 09:23:40 -0800

    add index.css and done login form

--- react-app/src/components/auth/loginForm/LoginForm.js
+++ react-app/src/components/auth/loginForm/LoginForm.js
@@ -33,37 +33,61 @@ const LoginForm = () => {
   }
 
   return (
-    <div className='login-form'>
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='login-page'>
+      <div className='login-form'>
+        <form onSubmit={onLogin}>
+          <div className='welcome-title'>
+            <h2>Welcome to Eriscord!</h2>
+          </div>
+          <div className='welcome-content'>
+            <h3>We're so excited to see you!</h3>
+          </div>
+          <div className='error-lists'>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className='e-p-input-div'>
+            <div className='e-font'>
+              <label htmlFor='email'>EMAIL</label>
+            </div>
+
+            <input
+              name='email'
+              type='text'
+              value={email}
+              onChange={updateEmail}
+            />
+
+
+          </div>
+          <div className='e-p-input-div'>
+            <div className='p-font'>
+              <label htmlFor='password'>PASSWORD</label>
+            </div>
+            <div>
+              <input
+                name='password'
+                type='password'
+                value={password}
+                onChange={updatePassword}
+              />
+            </div>
+
+            <div className='login-button'>
+              <button type='submit'>Log In</button>
+            </div>
+          </div>
+        </form>
+        <div className='Demo-button'>
+          <DemoUserLogin />
+        </div>
+        <div className='link-register'>
+          <span>Need an account?</span>
+          <span> <Link to={"/sign-up"}>Register</Link></span>
+        </div>
+
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
-    <Link to={"/sign-up"}>Register</Link>
-    <DemoUserLogin />
     </div>
   );
 };
