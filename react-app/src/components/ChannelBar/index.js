f28cba515d79329b7809ebdd208c96c448058880
dongfang <yusu.us@gmail.com>
2022-11-12 07:45:50 -0500

    channel bar

---
+++ react-app/src/components/ChannelBar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './ChannelBar.css';
+
+
+function ChannelBar(){
+    return (
+        <>
+        This gonna be the channle bar!
+        <div>
+            
+        </div>
+        </>
+    )
+}
+
+
+
+
+export default ChannelBar;
