6ad9c85b0f4a3ec1dee5e01bdda17e663e80a600
cici1819 <cchj1819@gmail.com>
2022-11-14 11:54:28 -0800

    merge test-cici

--- react-app/src/components/Channel/ChannelCreate/ChannelCreateForm.js
+++ react-app/src/components/Channel/ChannelCreate/ChannelCreateForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
+import { useParams } from "react-router";
 import { thunkAddChannelToServer } from "../../../store/channelReducer";
 import './ChannelCreate.css';
 
@@ -13,6 +14,7 @@ function ChannelCreate({ setShowModal }) {
     const [validationErrors, setValidationErrors] = useState([]);
     const [errors, setErrors] = useState([]);
     const history = useHistory();
+    const { channelId, serverId } = useParams();
 
     useEffect(() => {
         const errors = [];
